56b7521595e763e0373002abcde783f20188a3ef
Curtis Larsen <larsen@dixie.edu>
2015-05-23 22:44:14 -0600

    begin removing boost::serialization;  TicTacToe done

--- prog/TicTacToe/BoostExport.cpp
+++
@@ -1,6 +0,0 @@
-#include <ai_tictactoe.h>
-
-BOOST_CLASS_EXPORT_GUID(ai::TicTacToe::Action, "TicTacToeAction")
-BOOST_CLASS_EXPORT_GUID(ai::TicTacToe::Agent, "TicTacToeAgent")
-BOOST_CLASS_EXPORT_GUID(ai::TicTacToe::Environment, "TicTacToeEnvironment")
-BOOST_CLASS_EXPORT_GUID(ai::TicTacToe::Board, "TicTacToeBoard")
